eb7f46b5c0f5c755e863331f88c4b8dae1231854
Emmanuel Bonnin <emmanuelbonnin92@gmail.com>
2022-12-06 00:13:29 -0300

    galeriaaterminarcasi

--- js/script.js
+++ js/script.js
@@ -1,36 +1,33 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 let imagenes = [
-{img: './img/1.jpg'},
-{img: './img/2.jpg'},
-{img: './img/3.jpg'},
-{img: './img/4.jpg'},
-{img: './img/5.jpg'},
-{img: './img/6.jpeg'},
-{img: './img/7.jpg'},
-{img: './img/8.jpg'},
-{img: './img/9.jpg'},
-{img: './img/10.jpeg'},
-{img: './img/11.jpeg'},
-{img: './img/12.jpg'},
-{img: './img/13.jpg'},
-{img: './img/14.jpg'},
-{img: './img/15.jpg'},
-{img: './img/16.jpeg'},
-{img: './img/17.jpeg'},
-{img: './img/18.jpeg'},
-{img: './img/19.jpg'},
-{img: './img/20.jpeg'},
-{img: './img/21.jpeg'},
-{img: './img/22.jpeg'},
-{img: './img/23.jpeg'},
-{img: './img/24.jpeg'},
-{img: './img/25.jpeg'}
+    {img: './img/1.jpg'},
+    {img: './img/2.jpg'},
+    {img: './img/3.jpg'},
+    {img: './img/4.jpeg'},
+    {img: './img/5.jpeg'},
+    {img: './img/6.jpeg'},
+    {img: './img/7.jpeg'},
+    {img: './img/8.jpeg'},
+    {img: './img/9.jpeg'},
+    {img: './img/10.jpeg'},
+    {img: './img/11.jpg'},
+    {img: './img/12.jpg'},
+    {img: './img/13.jpg'},
+    {img: './img/14.jpeg'},
+    {img: './img/15.jpeg'},
+    {img: './img/16.jpeg'},
+    {img: './img/17.jpeg'},
+    {img: './img/18.jpeg'},
+    {img: './img/19.jpeg'},
+    {img: './img/20.jpeg'},
+   
 ]
 let contador = 0
 const contenedor = document.querySelector('.slideshow')
 const overlay = document.querySelector('.overlay')
 const galeria_imagenes = document.querySelectorAll('.galeria img')
+const galeria2_imagenes = document.querySelectorAll('.galeria2 img')
 const img_slideshows = document.querySelector('.slideshow img')
 
 contenedor.addEventListener('click', function(event) {
@@ -71,8 +68,23 @@ var span = document.getElementsByClassName("btn_cerrar")[0];
 span.onclick = function() {
     overlay.style.visibility = "hidden";
 }
+
+
+Array.from(galeria2_imagenes).forEach(img => {
+    img.addEventListener('click', event => {
+        const imagen_seleccionada = +(event.target.dataset.imgMostrar)
+        img_slideshows.src = imagenes[imagen_seleccionada].img
+        contador = imagen_seleccionada
+        overlay.style.opacity = 1
+        overlay.style.visibility = 'visible'
+    })
 })
 
+var span = document.getElementsByClassName("btn_cerrar")[0];
+span.onclick = function() {
+    overlay.style.visibility = "hidden";
+}
+})
 
 
 
